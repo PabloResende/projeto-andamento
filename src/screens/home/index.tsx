92221e731bb32597e40e6bacecdd0c090073efa9
pablo <wademcfild@gmail.com>
2023-08-31 15:04:59 -0300

    ...

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,12 +1,6 @@
 import "./style.css";
 import {
-  FaUser,
-  FaSearch,
-  FaUserPlus,
-  FaDownload,
-  FaInfoCircle,
-  FaBriefcase,
-  FaUsers,
+  FaUser
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logoWide from "../../assets/imgs/logoWide.png";
@@ -21,33 +15,13 @@ export function Home() {
           <div className="logo">
             <img src={logoWide} alt="lem urbanismo" />
           </div>
-          <ul id="navbar_lateral">
-            <li>
-              <a href="/">home</a>
-            </li>
-            <li>
-              <a href="#info_down_cad">informativo</a>
-            </li>
-            <li>
-              <a href="/">downloads</a>
-            </li>
-            <li>
-              <a href="../register">cadastro</a>
-            </li>
-          </ul>
-          <div className="search-bar">
-            <input type="text" placeholder="Pesquisar..." />
-            <button type="submit">
-              <FaSearch />
-            </button>
-          </div>
 
           <Link to="../Login" style={{ textDecoration: "none" }}>
             <div id="login">
-              <div className="circle">
+              <div className="bg-white p-2 rounded-full">
                 <FaUser size={25} color="3a28a" />
               </div>
-              <span>Login</span>
+              <span className="text-2xl">Login</span>
             </div>
           </Link>
         </nav>
@@ -75,82 +49,6 @@ export function Home() {
             </div>
           </section>
         </div>
-        <section id="conteudo_final">
-          <div className="info_down_cad">
-            <div className="info_item">
-              <a href="/">
-                <FaInfoCircle color="3a28a" />
-              </a>
-              <div className="info_text">
-                <h2>informativo</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                  diam nonummy nibh euismod tincidunt ut laoreet dolorediam
-                  nonummy nibh euismod tincidunt ut laoreet dolorediam nonummy
-                  nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                  volutpat.
-                </p>
-              </div>
-            </div>
-
-            <div className="info_item">
-              <a href="/">
-                <FaDownload color="3a28a" />
-              </a>
-              <div className="info_text">
-                <h2>download</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                  diam nonummy nibh euismod tincidunt ut laoreet dolorediam
-                  nonummy nibh euismod tincidunt ut laoreet dolorediam nonummy
-                  nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-                  volutpat.
-                </p>
-              </div>
-            </div>
-
-            <Link to="../register" style={{ textDecoration: "none" }}>
-              <div className="info_item">
-                <a href="">
-                  <FaUserPlus color="3a28a" />
-                </a>
-                <div className="info_text">
-                  <h2>cadastro</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-                    sed diam nonummy nibh euismod tincidunt ut laoreet
-                    dolorediam nonummy nibh euismod tincidunt ut laoreet
-                    dolorediam nonummy nibh euismod tincidunt ut laoreet dolore
-                    magna aliquam erat volutpat.
-                  </p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        </section>
-
-        <section>
-          <div className="area_prof_tra">
-            <div className="statistica"></div>
-            <div className="botao-container">
-              <a href="/" id="profissional-area">
-                <div className="circle-profissional">
-                  <FaBriefcase size={50} color="3a28a" />
-                </div>
-                <span>
-                  <h2>Área Profissional</h2>
-                </span>
-              </a>
-              <a href="/" id="work-area">
-                <div className="circle-work">
-                  <FaUsers size={60} color="3a28a" />
-                </div>
-                <h2>Área do Cliente</h2>
-              </a>
-            </div>
-          </div>
-        </section>
-
         <section>
           <div className="prefeitura">
             <img src={logoprefeitura} alt="logo prefeitura" />
