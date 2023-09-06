f1f2987898e7cdffa4024ca0e745947c989fa282
pablo <wademcfild@gmail.com>
2023-09-06 10:02:54 -0300

    ....

--- src/components/buttonRegister/index.tsx
+++ src/components/buttonRegister/index.tsx
@@ -1,2 +1,15 @@
+import React from 'react';
 
+interface ButtonProps {
+    title: string;
+    onClick: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ title, onClick }) => {
+    return (
+        <button onClick={onClick}>
+            {title}
+        </button>
+    );
+}
 
